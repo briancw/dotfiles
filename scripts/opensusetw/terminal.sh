4bf14e69fd3544108e958e861d5ef91391f19906
Brian Whicheloe <brian@whicheloe.us>
2022-02-02 13:42:25 -0800

    Add console output for each step

--- scripts/opensusetw/terminal.sh
+++ scripts/opensusetw/terminal.sh
@@ -2,10 +2,12 @@
 sudo zypper update -y
 
 # Install ZSH
+echo -e "\e[1;34m Install ZSH \e[0m"
 sudo zypper install -y zsh
 
 # Symlink dot files into home directory
 # Consider replacing this with gnu-stow
+echo -e "\e[1;34m Symlink dotfiles \e[0m"
 ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/zsh/.zshenv ~/.zshenv
 ln -s ~/.dotfiles/zsh/.aliases ~/.aliases
@@ -16,19 +18,24 @@ ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
 
 # Install FNM
+echo -e "\e[1;34m Install FNM \e[0m"
 curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
 
 # Install AntiBody
+echo -e "\e[1;34m Install AntiBody \e[0m"
 curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
 
 # Bundle ZSH plugins with antibody
+echo -e "\e[1;34m Bundle ZSH Plugins \e[0m"
 antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
 
 # Change default shell to Zsh
+echo -e "\e[1;34m Change default shell to ZSH \e[0m"
 sudo chsh -s $(which zsh) $USER
 
 # Create Projects Dir
 mkdir ~/Projects
 
 # Remove user bin folder
+echo -e "\e[1;34m Remove OpenSuse default user bin folder \e[0m"
 rm -rf ~/bin
